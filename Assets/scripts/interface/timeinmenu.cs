145219f9667983f6932bce9ee48e46b98e1553f9
NahuelHawk <nmuchetti@purpletreestudio.com>
2024-11-02 13:05:13 -0300

    pasada localizacion a obtencion asincrona para compatilidad con webgl

--- Assets/scripts/interface/timeinmenu.cs
+++ Assets/scripts/interface/timeinmenu.cs
@@ -2,15 +2,22 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.Localization;
+using System;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
 public class TimeInMenu : MonoBehaviour {
     [SerializeField] PointsCounter points;
     private Text text;
     [SerializeField] LocalizedString menu_best_time;
-
+    
     void Start () {
         text = GetComponent<Text>();
-        text.text = menu_best_time.GetLocalizedString() + ":\n " + points.bestPoints.ToString("F0");
-
+        var asyncString = menu_best_time.GetLocalizedStringAsync();
+        asyncString.Completed += OnStringCompleted;
     }
 
+    private void OnStringCompleted(AsyncOperationHandle<string> obj)
+    {
+        text.text = obj.Result +":\n " + points.bestPoints.ToString("F0");
+    }
 }
